3c01c4d8c473dab276437f4d6fdce554c6fd0fdb
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-01-19 04:57:13 +0300

    Create Solution.sql

---
+++ Solution.sql
@@ -0,0 +1,21 @@
+--SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+--SELECT first_name, last_name FROM actor WHERE first_name IN ('Penelope','Nick','Ed');
+--SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
+
+ENGLISH:
+--Hello,
+
+--Please code query scenarios that are below by using the example datatable dvdrental.
+
+--Query all of the columns of the film table if replacement_cost value is bigger than and equals to 12.99 AND smaller than and equals to 16.99 (Use BETWEEN - AND structure)
+--Query first_name and last_name columns that reside at actor table if first_name equals to 'Penelope', 'Nick' or 'Ed'. (Use IN keyword)
+--Query all of the columns of the film table if rental_rate consists of 0.99, 2.99, or 4.99 AND replacement_cost consists of 12.99, 15.99, 28.99. 
+
+TURKISH:
+--Merhabalar,
+
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+--actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+--film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
